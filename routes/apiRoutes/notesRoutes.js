2d05dc9284f9cec7849f1e2dc6d1d77d2ec8c447
raddymaster <antellitocci@gmail.com>
2021-06-26 17:56:28 -0400

    initial notes get api route

---
+++ routes/apiRoutes/notesRoutes.js
@@ -0,0 +1,9 @@
+const router = require('express').Router();
+//const { getNotes, saveNote, deleteNote } = require('../../public/assets/js/index');
+const { notes } = require('../../db/notes');
+
+router.get('/notes', (req, res) =>{
+    res.json(notes);
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
-//const apiRoutes = require('./routes/apiRoutes');
+const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
 
 //parse incoming string or array data
@@ -13,7 +13,7 @@ app.use(express.json());
 //serve static pages in public
 app.use(express.static('public'));
 
-//app.use('/api', apiRoutes);
+app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
 
 app.listen(PORT, () => {
