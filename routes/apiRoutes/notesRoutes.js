a8b6207f9afae481850e8bd2ff7151929e802b7c
raddymaster <antellitocci@gmail.com>
2021-06-27 11:20:23 -0400

    create and save new note functionality implemented. Render save notes also implemented

--- routes/apiRoutes/notesRoutes.js
+++ routes/apiRoutes/notesRoutes.js
@@ -1,11 +1,22 @@
 const router = require('express').Router();
-//const { getNotes, saveNote, deleteNote } = require('../../public/assets/js/index');
-const { notes } = require('../../db/notes');
+const { createNewNote } = require('../../lib/notes');
+const { notes } = require('../../db/notes.json');
 
 router.get('/notes', (req, res) =>{
     res.json(notes);
 });
 
-//POST goes here
+router.post('/notes', (req, res) => {
+    //give request an id based on its position in the array or notes
+    req.body.id = notes.length.toString();
+
+    //we don't need to validate here because it is performed on the client side
+    //the save button doesn't appear until both required form sections are filled
+    
+    //create new note
+    const note = createNewNote(req.body, notes);
+    res.json(note);
+
+});
 
 module.exports = router;
